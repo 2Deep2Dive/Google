5fd312df7871a01569fcde38fdd7bd494bbda4e8
Mohamed Eliwa <mohamed.eliwa@outlook.com>
2022-01-23 22:28:48 +0100

    updated scripts

--- google_automation_course2_final_project.py
+++ google_automation_course2_final_project.py
@@ -2,6 +2,24 @@
 import re
 import sys
 import os
+from unittest import result
 
-logs=$(grep ticky syslog.log)
-echo $logs
+logfile = sys.argv[1]
+
+def get_error_messages(logfile):
+    erros = {}
+    per_user = {}
+    errorPattern =  r"(.*)(ERROR)(.*\s).*\(([\w]+)\)"   #r"ticky: ERROR: ([\w ]*)"
+    infoPattern = r"(.*)(INFO)(.*\s).*\(([\w]+)\)"
+    try:
+        with open(logfile) as file:
+            for line in file:
+                if "ticky: INFO: [\w ]*)" in line :
+                    result = re.search(errorPattern, line)
+                    
+                else:
+                    continue
+            file.close()     
+    except:
+        print("Error")
+get_error_messages(logfile)

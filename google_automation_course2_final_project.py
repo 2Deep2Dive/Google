f485241ec5dace1fad8efe73b6d55aecec79d318
Mohamed Eliwa <mohamed.eliwa@outlook.com>
2022-01-23 22:31:47 +0100

    new RegExp

--- google_automation_course2_final_project.py
+++ google_automation_course2_final_project.py
@@ -9,17 +9,17 @@ logfile = sys.argv[1]
 def get_error_messages(logfile):
     erros = {}
     per_user = {}
-    errorPattern =  r"(.*)(ERROR)(.*\s).*\(([\w]+)\)"   #r"ticky: ERROR: ([\w ]*)"
-    infoPattern = r"(.*)(INFO)(.*\s).*\(([\w]+)\)"
+    errorPattern =  r"(.*)(ERROR): ([a-zA-Z ]*)(.*)\(([a-z]+)\)"   #r"ticky: ERROR: ([\w ]*)"
+    infoPattern = r"(.*)(INFO): ([a-zA-Z ]*)(.*)\(([a-z]+)\)"
     try:
         with open(logfile) as file:
             for line in file:
                 if "ticky: INFO: [\w ]*)" in line :
                     result = re.search(errorPattern, line)
-                    
+
                 else:
                     continue
-            file.close()     
+            file.close()
     except:
         print("Error")
 get_error_messages(logfile)

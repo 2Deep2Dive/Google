26aee3ffd106e1ad010185cae84c592f86523cdc
Mohamed Eliwa <mohamed.eliwa@outlook.com>
2022-01-24 22:57:46 +0100

    both dict one loop

--- google_automation_course2_final_project.py
+++ google_automation_course2_final_project.py
@@ -8,32 +8,6 @@ from unittest import result
 
 logfile = sys.argv[1]
 
-def get_errors(logfile):
-    error = {}
-    per_user = {}
-    errorPattern =  r"(.*)(ERROR): ([a-zA-Z ]*)(.*)\(([\w.]+)\)"   #r"ticky: ERROR: ([\w ]*)"
-    infoPattern = r"(.*)(INFO): ([a-zA-Z ]*)(.*)\(([\w.]+)\)"
-    with open(logfile) as file:
-        for line in file:
-            if "ticky: ERROR:" in line :
-                errorResult = re.search(errorPattern, line)
-                if (errorResult[3]) not in error:
-                    error[errorResult[3]] = 0
-                else:
-                    error[errorResult[3]] += 1    
-            elif "ticky: INFO:" in line:
-                infoResult = re.search(infoPattern, line)
-                if (infoResult[5]) not in per_user:
-                    per_user[infoResult[5]] = 0
-                else:    
-                    per_user[infoResult[5]] += 1
-            else:
-                continue
-        file.close()
-        sortedError = sorted(error.items(), key=operator.itemgetter(1), reverse=True)
-        sortedPerUser = sorted(per_user.items())
-    return print(sortedError), print(sortedPerUser)
-
 def get_results(logfile):
     error = {}
     per_user = {}
@@ -42,20 +16,20 @@ def get_results(logfile):
         for line in file:
                 result = re.search(Pattern, line)
                 if(result[2] == "ERROR"):
-                    print(result[2])
                     if (result[3]) not in error:
                         error[result[3]] = 1
                     else:
                         error[result[3]] += 1
                     if (result[5]) not in per_user:
                         per_user[result[5]] = {}
-                        per_user[result[5]][result[2]] = 1 
+                    if (result[2]) not in per_user[result[5]]:
+                        per_user[result[5]][result[2]] = 1
                     else:
-                        per_user[result[5]][result[2]] += 1    
+                        per_user[result[5]][result[2]] += 1   
                 elif (result[2] == "INFO"):
-                    print(result[2])
                     if (result[5] not in per_user):
                         per_user[result[5]] = {}
+                    if (result[2]) not in per_user[result[5]]:
                         per_user[result[5]][result[2]] = 1
                     else:
                         per_user[result[5]][result[2]] += 1

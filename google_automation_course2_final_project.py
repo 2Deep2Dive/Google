a815c32a7c004429bb0a39b353d0e909a6dbd2c4
Mohamed Eliwa <mohamed.eliwa@outlook.com>
2022-01-25 23:34:36 +0100

    2 new fucntions

--- google_automation_course2_final_project.py
+++ google_automation_course2_final_project.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
 import re
-import sys
 import os
+import csv
+import sys
 import operator
 
 from unittest import result
 
 logfile = sys.argv[1]
+
+def dict_to_list(dict):
+    list_from_dict = []
+    for k1,d in dict.items():
+        key = k1
+        for k2,v in d.items():
+            subKey = k2
+            value  = v
+        list_from_dict.append(tuple((key,subKey,value)))
+    return list_from_dict
+
+def list_to_csv(fileName, list):
+    with open(fileName, 'w',newline='') as csv_File:
+        writer = csv.writer(csv_File)
+        writer.writerows(list)
+    csv_File.close()
+    return
 
 def get_results(logfile):
     error = {}
@@ -25,7 +43,7 @@ def get_results(logfile):
                     if (result[2]) not in per_user[result[5]]:
                         per_user[result[5]][result[2]] = 1
                     else:
-                        per_user[result[5]][result[2]] += 1   
+                        per_user[result[5]][result[2]] += 1
                 elif (result[2] == "INFO"):
                     if (result[5] not in per_user):
                         per_user[result[5]] = {}
@@ -35,9 +53,13 @@ def get_results(logfile):
                         per_user[result[5]][result[2]] += 1
                 else:
                     continue
-        file.close()
-        sortedError = sorted(error.items(), key=operator.itemgetter(1), reverse=True)
-        sortedPerUser = sorted(per_user.items())
-    return print(sortedError), print(sortedPerUser)
+    file.close()
+    sortedError   = sorted(error.items(), key=operator.itemgetter(1), reverse=True)
+    sortedPerUser = sorted(dict_to_list(per_user))
+    sortedError.insert(0, tuple(("Error", "Count")))
+    sortedPerUser.insert(0,tuple(("Username", "INFO", "ERROR")))
+    list_to_csv("error_message.csv",sortedError)
+    list_to_csv("user_statistics.csv",sortedPerUser)
+    return
 
 get_results(logfile)
